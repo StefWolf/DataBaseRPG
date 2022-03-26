54d5e8ae2c8daad312055c328324f11ce959832f
StefWolf <stefaneassisori@gmail.com>
2022-03-26 10:19:07 -0300

    Inserindo dados na tabela de bioma

---
+++ DadosTeste/Biomas.sql
@@ -0,0 +1,10 @@
+
+
+
+INSERT INTO bioma VALUES (NULL, 'Floresta de Pinheiros', 'Extensa com alguns relevos elevados e árvores de pinheiro com solo quase todo de terra molhada e pedras com musgos. ');
+INSERT INTO bioma VALUES (NULL, 'Deserto', 'Plano com dunas de areia e uma área tão quente e seca no meio de um ambiente aberto.');
+INSERT INTO bioma VALUES (NULL, 'Floresta de Carvalhos', 'Extensa área viva e diversificada de árvores de carvalho, plantas e solo com grama molhada.');
+INSERT INTO bioma VALUES (NULL, 'Campo', 'Área plana e aberta cheia de grama e algumas flores.');
+INSERT INTO bioma VALUES (NULL, 'Floresta Temperada', 'Ocorre em ambiente de clima temperado e com as quatro estações bem definidas.');
+
+
