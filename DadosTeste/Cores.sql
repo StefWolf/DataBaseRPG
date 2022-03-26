6a8348192b4f0f5b6f9accf5715bd99f55c88ab7
StefWolf <stefaneassisori@gmail.com>
2022-03-26 10:10:54 -0300

    Inserindo alguns dados na tabela de cores

---
+++ DadosTeste/Cores.sql
@@ -0,0 +1,11 @@
+INSERT INTO cores VALUES (NULL, '#000000', 'Preto');
+INSERT INTO cores VALUES (NULL, '#808080', 'Cinza');
+INSERT INTO cores VALUES (NULL, '#0000FF', 'Azul');
+INSERT INTO cores VALUES (NULL, '#ADD8E6', 'Azul Claro');
+INSERT INTO cores VALUES (NULL, '#008000', 'Verde');
+INSERT INTO cores VALUES (NULL, '#A020F0', 'Roxo');
+INSERT INTO cores VALUES (NULL, '#FF0000', 'Vermelho');
+INSERT INTO cores VALUES (NULL, '#ffff00', 'Amarelo');
+INSERT INTO cores VALUES (NULL, ' #FFF8DC', 'Marrom');
+INSERT INTO cores VALUES (NULL, '#FFFFFF', 'Branco');
+INSERT INTO cores VALUES (NULL, '#FF7F00', 'Laranja')
