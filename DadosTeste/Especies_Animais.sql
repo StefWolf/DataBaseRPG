f34c3687c35f406c7fc87a8985803df1c2fd582a
StefWolf <stefaneassisori@gmail.com>
2022-03-27 07:26:49 -0300

    Adicionando alguns dados na tabela Especies_Animais

---
+++ DadosTeste/Especies_Animais.sql
@@ -0,0 +1,5 @@
+INSERT INTO especie_animais VALUES (NULL, 'Ovelha', 'Animal quadrúpede de pelagem que pode variar do branco para o preto. Muito utilizados pelos fazendeiros que os adestram para retirada de lã. Seu alimento principal é sementes e capim.  ','Herbívoro');
+INSERT INTO especie_animais VALUES (NULL, 'Cavalo', 'Animal quadrúpede utilizado muito pelos humanos como monstaria','Herbívoro');
+INSERT INTO especie_animais VALUES (NULL, 'Lobo', 'Animal que vive em grupo, peludo de focinho longo e dentes pontudos e caninos.','Carnívoro');
+INSERT INTO especie_animais VALUES (NULL, 'Porco', 'Animal quadrúpede muito utilizado para o consumo humano.','Herbívoro');
+INSERT INTO especie_animais VALUES (NULL, 'Peixe', 'Animal aquático com nadadeiras','Ornívoro');
